d67cdb7d05dd6fac17f71b349fb6f6b47639a872
George Yang <gzyang4@gmail.com>
2022-05-04 13:07:46 -0400

    cleaning directories

--- 1 clean_GED_event.R
+++ 1 clean_GED_event.R
@@ -1,6 +1,5 @@
-setwd("C:/Users/user/Dropbox/CGD/Projects/dem_neg_labor/input/ucdp_war")
-
-load("GEDEvent_v21_1.RData")
+setwd(input_dir)
+load("ucdp_war/GEDEvent_v21_1.RData")
 df <- as.data.table(GEDEvent_v21_1)
 df <- df[, .(year, country, best)]
 df[, iso3c := name2code(country, custom_match = c("Yemen (North Yemen)" = "YEM"))]
@@ -9,6 +8,4 @@ df.exp <- CJ(year = seq(min(df$year), max(df$year), by = 1), iso3c = unique(df$i
 df <- merge(df.exp, df, by = c("iso3c", "year"), all.x = TRUE)
 df[is.na(est_deaths),est_deaths:=0]
 df <- df[order(iso3c, year)]
-
-setwd("C:/Users/user/Dropbox/CGD/Projects/dem_neg_labor/input/")
 readstata13::save.dta13(df, "UCDP_geography_deaths.dta")
