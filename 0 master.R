980751f3758aa1170e90448671a4576a8fee8db0
CGDev <gyang@cgdev.org>
2022-05-10 16:59:21 -0400

    add run everything chunk

--- 0 master.R
+++ 0 master.R
@@ -19,21 +19,23 @@ user<-Sys.info()["user"]
 root_dir <- paste0("C:/Users/", user, "/Dropbox/CGD/Projects/dem_neg_labor/")
 input_dir <- paste0(root_dir, "input")
 output_dir <- paste0(root_dir, "output")
-code_dir <- paste0(root_dir, "code")
+code_dir <- paste0(root_dir, "labor-growth")
+overleaf_dir <- glue("C:/Users/{user}/Dropbox/Apps/Overleaf/Demographic Labor Effects")
 
 setwd(input_dir)
 
 # Packages ---------------------------------------------------------------
 list.of.packages <- c( "base", "car", "cowplot", "dplyr", "ggplot2",
     "ggthemes", "graphics", "grDevices", "grid", "gridExtra", "gvlma", "h2o",
-        "lubridate", "MASS", "readxl", "rio", "rms", "rsample", "stats",
-        "tidyr", "utils", "zoo", "xtable", "stargazer", "data.table",
-        "ggrepel", "foreign", "fst", "countrycode", "wbstats", "quantmod",
-        "R.utils", "leaps", "bestglm", "dummies", "caret", "jtools",
-        "huxtable", "haven", "ResourceSelection", "betareg", "quantreg",
-        "margins", "plm", "collapse", "kableExtra", "tinytex", "LambertW",
-        "scales", "stringr", "imputeTS", "shadowtext", "pdftools", "glue",
-        "purrr", "OECD", "RobustLinearReg", "forcats", "WDI", "xlsx", "readstata13")
+    "lubridate", "MASS", "readxl", "rio", "rms", "rsample", "stats",
+    "tidyr", "utils", "zoo", "xtable", "stargazer", "data.table",
+    "ggrepel", "foreign", "fst", "countrycode", "wbstats", "quantmod",
+    "R.utils", "leaps", "bestglm", "dummies", "caret", "jtools",
+    "huxtable", "haven", "ResourceSelection", "betareg", "quantreg",
+    "margins", "plm", "collapse", "kableExtra", "tinytex", "LambertW",
+    "scales", "stringr", "imputeTS", "shadowtext", "pdftools", "glue",
+    "purrr", "OECD", "RobustLinearReg", "forcats", "WDI", "xlsx",
+    "readstata13")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if (length(new.packages)) install.packages(new.packages, dependencies = TRUE)
@@ -47,3 +49,12 @@ theme_set(theme_clean() +
 # source(paste0(root_dir, "code/", "helper_functions.R"))
 source(paste0("C:/Users/", user, "/Dropbox/Coding_General/personal.functions.R"))
 
+# Run everything ----------------------------------------------------------
+
+# create log directory path:
+dir.create(file.path(root_dir, "log"), showWarnings = FALSE)
+
+closeAllConnections()
+setwd(input_dir)
+sink(file=glue("{root_dir}log/log_3 graphs.txt")); source(glue("{code_dir}/3 graphs.R"), echo=TRUE, max.deparse.length=10000); sink()
+setwd(input_dir)
