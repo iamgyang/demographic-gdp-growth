52da65fc03adebcb28db57ab73d52e6811e4e1b5
CGDev <gyang@cgdev.org>
2022-05-18 14:28:33 -0400

    init.

--- 0 master.R
+++ 0 master.R
@@ -14,15 +14,6 @@ options(datatable.auto.index = FALSE)
 # clear environment objects
 rm(list = ls())
 
-# You will have to edit this to be your own computer's working directories:
-user<-Sys.info()["user"]
-root_dir <- paste0("C:/Users/", user, "/Dropbox/CGD/Projects/dem_neg_labor/")
-input_dir <- paste0(root_dir, "input")
-output_dir <- paste0(root_dir, "output")
-code_dir <- paste0(root_dir, "labor-growth")
-overleaf_dir <- glue("C:/Users/{user}/Dropbox/Apps/Overleaf/Demographic Labor Effects")
-
-setwd(input_dir)
 
 # Packages ---------------------------------------------------------------
 list.of.packages <- c( "base", "car", "cowplot", "dplyr", "ggplot2",
@@ -45,6 +36,18 @@ for (package in list.of.packages) {library(eval((package)), character.only = TRU
 theme_set(theme_clean() + 
               theme(plot.background = 
                         element_rect(color = "white")))
+
+# Directories -------------------------------------------------------------
+
+# You will have to edit this to be your own computer's working directories:
+user<-Sys.info()["user"]
+root_dir <- paste0("C:/Users/", user, "/Dropbox/CGD/Projects/dem_neg_labor/")
+input_dir <- paste0(root_dir, "input")
+output_dir <- paste0(root_dir, "output")
+code_dir <- paste0(root_dir, "labor-growth")
+overleaf_dir <- glue("C:/Users/{user}/Dropbox/Apps/Overleaf/Demographic Labor Effects")
+
+setwd(input_dir)
 
 # source(paste0(root_dir, "code/", "helper_functions.R"))
 source(paste0("C:/Users/", user, "/Dropbox/Coding_General/personal.functions.R"))
